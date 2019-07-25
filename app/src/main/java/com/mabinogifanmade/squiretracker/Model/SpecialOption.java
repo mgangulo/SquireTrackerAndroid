e5a0b8b40a815da46ae336948bba6d4bcdecba7b
mgangulo <gaby9415@hotmail.com>
2019-07-25 11:41:36 -0400

    Model Skeleton

---
+++ app/src/main/java/com/mabinogifanmade/squiretracker/Model/SpecialOption.java
@@ -0,0 +1,31 @@
+package com.mabinogifanmade.squiretracker.Model;
+
+public class SpecialOption {
+    private String convoText;
+    private String hint;
+    float percent;
+
+    public String getConvoText() {
+        return convoText;
+    }
+
+    public void setConvoText(String convoText) {
+        this.convoText = convoText;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    public float getPercent() {
+        return percent;
+    }
+
+    public void setPercent(float percent) {
+        this.percent = percent;
+    }
+}
