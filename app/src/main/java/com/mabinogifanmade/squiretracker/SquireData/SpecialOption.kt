b44287c42e0d426148eb73ea1a71b1f013c16b81
mgangulo <gaby9415@hotmail.com>
2019-07-26 16:17:35 -0400

    Translate for one word

--- app/src/main/java/com/mabinogifanmade/squiretracker/SquireData/SpecialOption.kt
+++
@@ -1,9 +0,0 @@
-package com.mabinogifanmade.squiretracker.SquireData
-
-class SpecialOption(convoText: String, hint: String, percent: Float) {
-    var convoText: String? = convoText
-    var hint: String? = hint
-    var percent: Float = percent
-
-    constructor(convoText: String, percent: Float):this(convoText,"",percent){}
-}
--- app/src/main/java/com/mabinogifanmade/squiretracker/utils/ConversationUtils.kt
+++ app/src/main/java/com/mabinogifanmade/squiretracker/utils/ConversationUtils.kt
@@ -1,8 +1,29 @@
 package com.mabinogifanmade.squiretracker.utils
 
+import com.mabinogifanmade.squiretracker.squiredata.Conversation
+
 class ConversationUtils {
-    companion object{
-        fun translate():String{
+    companion object {
+        fun translate(key: String): String {
+            when (key) {
+                Conversation.AWKWARD_ABV -> return Conversation.AWKWARD
+
+                Conversation.MISSION_ABV -> return Conversation.MISSION
+
+                Conversation.TRAINING_ABV -> return Conversation.TRAINING
+
+                Conversation.PLAYING_ABV -> return Conversation.PLAYING
+
+                Conversation.COOKING_ABV -> return Conversation.COOKING
+
+                Conversation.FASHION_ABV -> return Conversation.FASHION
+
+                Conversation.DATING_ABV -> return Conversation.DATING
+
+                Conversation.SERIOUS_ABV -> return Conversation.SERIOUS_ABV
+
+                Conversation.FUN_ABV -> return Conversation.FUN_ABV
+            }
             return ""
         }
     }
