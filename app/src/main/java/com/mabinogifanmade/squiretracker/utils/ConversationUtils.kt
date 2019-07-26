3593979d77ae6fc19c87df0be0f64a8394ba134d
mgangulo <gaby9415@hotmail.com>
2019-07-26 16:19:55 -0400

    Translation inverse

--- app/src/main/java/com/mabinogifanmade/squiretracker/utils/ConversationUtils.kt
+++ app/src/main/java/com/mabinogifanmade/squiretracker/utils/ConversationUtils.kt
@@ -4,7 +4,7 @@ import com.mabinogifanmade.squiretracker.squiredata.Conversation
 
 class ConversationUtils {
     companion object {
-        fun translate(key: String): String {
+        fun translateAbv(key: String): String {
             when (key) {
                 Conversation.AWKWARD_ABV -> return Conversation.AWKWARD
 
@@ -20,9 +20,32 @@ class ConversationUtils {
 
                 Conversation.DATING_ABV -> return Conversation.DATING
 
-                Conversation.SERIOUS_ABV -> return Conversation.SERIOUS_ABV
+                Conversation.SERIOUS_ABV -> return Conversation.SERIOUS
 
-                Conversation.FUN_ABV -> return Conversation.FUN_ABV
+                Conversation.FUN_ABV -> return Conversation.FUN
+            }
+            return ""
+        }
+
+        fun translateOption(key: String): String {
+            when (key) {
+                Conversation.AWKWARD -> return Conversation.AWKWARD_ABV
+
+                Conversation.MISSION -> return Conversation.MISSION_ABV
+
+                Conversation.TRAINING -> return Conversation.TRAINING_ABV
+
+                Conversation.PLAYING -> return Conversation.PLAYING_ABV
+
+                Conversation.COOKING -> return Conversation.COOKING_ABV
+
+                Conversation.FASHION -> return Conversation.FASHION_ABV
+
+                Conversation.DATING -> return Conversation.DATING_ABV
+
+                Conversation.SERIOUS -> return Conversation.SERIOUS_ABV
+
+                Conversation.FUN -> return Conversation.FUN_ABV
             }
             return ""
         }
