0f2fe95e314bb699fa3339c5f0239f1e388d56db
mgangulo <gaby9415@hotmail.com>
2019-08-23 16:04:16 -0400

    Prettier button

--- app/src/main/java/com/mabinogifanmade/squiretracker/adapters/PlayerAdapter.kt
+++ app/src/main/java/com/mabinogifanmade/squiretracker/adapters/PlayerAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
 import com.mabinogifanmade.squiretracker.R
 import com.mabinogifanmade.squiretracker.userdata.PlayerChar
@@ -27,17 +28,17 @@ class PlayerAdapter(val playerChars: ArrayList<PlayerChar>,val currentChar:Playe
         val character: PlayerChar = playerChars.get(position)
         holder.characterName.setText(character.charName)
         holder.characterServer.setText(character.server)
-        holder.switchButton.visibility = when (character.equals(currentChar)){
+        /*holder.switchButton.visibility = when (character.equals(currentChar)){
             true -> View.INVISIBLE
             else -> View.VISIBLE
-        }
+        }*/
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val editButton:ImageButton = itemView.findViewById(R.id.editButton)
         val deleteButton:ImageButton = itemView.findViewById(R.id.deleteButton)
-        val switchButton: Button = itemView.findViewById(R.id.switchButton)
+        val switchButton: AppCompatButton = itemView.findViewById(R.id.switchButton)
         val characterName: TextView = itemView.findViewById(R.id.charName)
         val characterServer: TextView = itemView.findViewById(R.id.charServer)
         val charAvatar: ImageView = itemView.findViewById(R.id.characterAvatar)
