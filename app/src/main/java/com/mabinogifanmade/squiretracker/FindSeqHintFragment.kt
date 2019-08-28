dcd36b4849d50fd5e70123c609e20e09924f67a0
mgangulo <gaby9415@hotmail.com>
2019-08-28 18:20:15 -0400

    Test stream

--- app/src/main/java/com/mabinogifanmade/squiretracker/FindSeqHintFragment.kt
+++ app/src/main/java/com/mabinogifanmade/squiretracker/FindSeqHintFragment.kt
@@ -1,11 +1,16 @@
 package com.mabinogifanmade.squiretracker
 
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.mabinogifanmade.squiretracker.squiredata.Squire
+import com.mabinogifanmade.squiretracker.utils.ConversationUtils
+import java.util.stream.Collectors
 
 
 /**
@@ -13,6 +18,8 @@ import androidx.fragment.app.Fragment
  *
  */
 class FindSeqHintFragment : Fragment() {
+    val args: FindSeqHintFragmentArgs by navArgs()
+    private lateinit var squire: Squire
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +29,15 @@ class FindSeqHintFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_find_seq_hint, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        squire = args.squire
+        val hintSpinnerList: ArrayList<String> = arrayListOf(getString(R.string.select_text))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            hintSpinnerList.addAll(squire.sequenceHint
+                .chars().distinct()
+                .mapToObj({ c -> ConversationUtils.translateAbv(c as Char) })
+                .collect(Collectors.toList()))
+        }
+    }
 }
