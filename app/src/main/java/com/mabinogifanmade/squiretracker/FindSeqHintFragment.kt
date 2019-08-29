56695c5d4d072cf521044519b92ebcfa829c9157
mgangulo <gaby9415@hotmail.com>
2019-08-29 10:59:56 -0400

    Changing tab layout to linear due to children width issues. Adding other things to view

--- app/src/main/java/com/mabinogifanmade/squiretracker/FindSeqHintFragment.kt
+++ app/src/main/java/com/mabinogifanmade/squiretracker/FindSeqHintFragment.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.mabinogifanmade.squiretracker.squiredata.Squire
 import com.mabinogifanmade.squiretracker.utils.ConversationUtils
+import kotlinx.android.synthetic.main.fragment_find_seq_hint.*
 import java.util.stream.Collectors
 
 
@@ -34,10 +36,29 @@ class FindSeqHintFragment : Fragment() {
         squire = args.squire
         val hintSpinnerList: ArrayList<String> = arrayListOf(getString(R.string.select_text))
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            hintSpinnerList.addAll(squire.sequenceHint
-                .chars().distinct()
-                .mapToObj({ c -> ConversationUtils.translateAbv(c as Char) })
-                .collect(Collectors.toList()))
+            hintSpinnerList.addAll(
+                squire.sequenceHint
+                    .chars().distinct()
+                    .mapToObj({ c -> ConversationUtils.translateAbv(c.toChar()) })
+                    .collect(Collectors.toList())
+            )
+        } else {
+            val charSet: Set<Char> = squire.sequenceHint.toCharArray().toSet()
+            for (c in charSet) {
+                hintSpinnerList.add(ConversationUtils.translateAbv(c))
+            }
+        }
+        ArrayAdapter<String>(
+            context!!,
+            android.R.layout.simple_spinner_item,
+            hintSpinnerList
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            hintSpinner1.adapter = it
+            hintSpinner2.adapter = it
+            hintSpinner3.adapter = it
+            hintSpinner4.adapter = it
+            hintSpinner5.adapter = it
         }
     }
 }
